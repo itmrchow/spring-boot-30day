85ac0733bc43b956af9fa7641dbac73a44723715
Jeff <itmrchow@gmail.com>
2020-11-02 15:06:41 +0800

    spring init

---
+++ springboot30day/src/main/java/com/springboot30day/springboot30day/Springboot30dayApplication.java
@@ -0,0 +1,13 @@
+package com.springboot30day.springboot30day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot30dayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot30dayApplication.class, args);
+	}
+
+}
---
+++ springboot30day/src/test/java/com/springboot30day/springboot30day/Springboot30dayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot30day.springboot30day;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot30dayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
