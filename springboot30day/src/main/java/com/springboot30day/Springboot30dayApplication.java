049419221c78014f8a335296932554fdea3fba7a
Jeff <itmrchow@gmail.com>
2020-11-03 11:53:18 +0800

    Swagger

--- springboot30day/src/main/java/com/springboot30day/Springboot30dayApplication.java
+++ springboot30day/src/main/java/com/springboot30day/Springboot30dayApplication.java
@@ -3,6 +3,7 @@ package com.springboot30day;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Springboot30dayApplication {
 
