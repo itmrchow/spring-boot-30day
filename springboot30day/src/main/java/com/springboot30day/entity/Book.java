e9098339f0bfe8df8b338391bda9842e8485bf26
Jeff <itmrchow@gmail.com>
2020-11-03 11:52:55 +0800

    key add GenerationType.IDENTITY

--- springboot30day/src/main/java/com/springboot30day/entity/Book.java
+++ springboot30day/src/main/java/com/springboot30day/entity/Book.java
@@ -2,6 +2,7 @@ package com.springboot30day.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Data;
@@ -10,9 +11,8 @@ import lombok.Data;
 @Entity
 public class Book {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  
     private Integer bookid;
     private String name;
     private String author;
-
 }
