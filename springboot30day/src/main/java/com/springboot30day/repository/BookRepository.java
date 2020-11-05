058959f5ddb3fcd6a491213b29e38bd81838afcc
Jeff <itmrchow@gmail.com>
2020-11-05 15:45:11 +0800

    Unit Test

--- springboot30day/src/main/java/com/springboot30day/repository/BookRepository.java
+++ springboot30day/src/main/java/com/springboot30day/repository/BookRepository.java
@@ -2,27 +2,12 @@ package com.springboot30day.repository;
 
 import com.springboot30day.entity.Book;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource
 public interface BookRepository extends JpaRepository<Book, Integer> {
-
-    // Prevents GET /books/:id
-
-    @Override
-    Book getOne(Integer id);
-
-    // Prevents GET /books
-    @Override
-    Page<Book> findAll(Pageable pageable);
-
-    // Prevents POST /books and PATCH /books/:id
-    @Override
-    Book save(Book s);
 
     // Prevents DELETE /books/:id
     @Override
--- springboot30day/src/test/java/com/springboot30day/springboot30day/Springboot30dayApplicationTests.java
+++ springboot30day/src/test/java/com/springboot30day/springboot30day/Springboot30dayApplicationTests.java
@@ -1,13 +1,71 @@
 package com.springboot30day.springboot30day;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
-@SpringBootTest
+import com.springboot30day.Springboot30dayApplication;
+import com.springboot30day.entity.Book;
+import com.springboot30day.repository.BookRepository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import static junit.framework.TestCase.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Springboot30dayApplication.class,
+		// WebEnvironment:啟動class web環境
+		// WebEnvironment.MOCK—提供一個Mock的Servlet環境，內置的Servlet容器並沒有真實的啟動，主要搭配使用@AutoConfigureMockMvc
+		// WebEnvironment.RANDOM_PORT — 提供一個真實的Servlet環境，也就是說會啟動內置容器，然後使用的是隨機端口
+		// WebEnvironment.DEFINED_PORT — 這個配置也是提供一個真實的Servlet環境，使用的默認的端口，如果沒有配置就是8080
+		// WebEnvironment.NONE — 這是個神奇的配置，跟Mock一樣也不提供真實的Servlet環境。
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Springboot30dayApplicationTests {
 
+	@Autowired
+	private BookRepository bookRepository;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 	@Test
-	void contextLoads() {
+	public void testGetBook() {
+		// insert
+		Book book = new Book();
+		book.setName("被討厭的勇氣：自我啟發之父「阿德勒」的教導");
+		book.setAuthor("岸見一郎");
+		book = bookRepository.save(book);
+
+		//url
+		UriComponentsBuilder builder = UriComponentsBuilder.fromPath("/api/v1/book/{bookid}");
+		//參數
+		Map<String, Object> uriParams = new HashMap<String, Object>();
+		uriParams.put("bookid", book.getBookid());
+
+		//head
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+		//實體
+		HttpEntity<Object> entity = new HttpEntity<>(headers);
+		
+		//送出
+		ResponseEntity<String> response = restTemplate.exchange(builder.buildAndExpand(uriParams).toUri().toString(),
+				HttpMethod.GET, entity, String.class);
+
+		assertTrue("testGetBook Fail:\n" + response.getBody(), response.getStatusCode().is2xxSuccessful());
 	}
 
 }
