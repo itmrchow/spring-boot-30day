30e460fe795e10b5008a84c779350e47981755b3
Jeff <itmrchow@gmail.com>
2020-11-11 23:23:12 +0800

    Cache

--- springboot30day/src/main/java/com/springboot30day/Springboot30dayApplication.java
+++ springboot30day/src/main/java/com/springboot30day/Springboot30dayApplication.java
@@ -2,8 +2,10 @@ package com.springboot30day;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.retry.annotation.EnableRetry;
 
+@EnableCaching//啟動快取
 @EnableRetry //啟動retry
 @SpringBootApplication
 public class Springboot30dayApplication {
--- springboot30day/src/main/java/com/springboot30day/component/ApplicationLoader.java
+++ springboot30day/src/main/java/com/springboot30day/component/ApplicationLoader.java
@@ -1,6 +1,9 @@
 package com.springboot30day.component;
 
+import java.text.SimpleDateFormat;
+
 import com.springboot30day.service.BookService;
+import com.springboot30day.service.TimeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,18 +11,29 @@ import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
-//@Slf4j
+@Slf4j
 @Component
+//在application執行時執行
 public class ApplicationLoader implements CommandLineRunner {
     @Autowired
     private BookService bookService;
+    @Autowired
+    private TimeService timeService;
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
     @Override
     public void run(String... args) throws Exception {
-        try {
-            bookService.getBook();
-        } catch (Exception e) {
-            e.printStackTrace();
+        // try {
+        //     bookService.getBook();
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
+
+        for (int i = 0; i < 30; i++) {
+            //cache 設定 5秒 -> 5秒後才會new新的time
+            log.info(sdf.format(timeService.getTime()));
+            Thread.sleep(1000);
         }
     }
 
